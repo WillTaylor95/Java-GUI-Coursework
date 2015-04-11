6f7572fdb773dddb66112db143006b5da3fdf93c
WillTaylor95 <will.s.taylor95@gmail.com>
2015-04-11 19:34:22 +0100

    added action listeners

--- src/CreateProfilePanel.java
+++ src/CreateProfilePanel.java
@@ -1,7 +1,58 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Will on 11/04/2015.
  */
 public class CreateProfilePanel extends JPanel {
+
+    private JLabel selectCourseJL;
+    private JTextField selectCourseTF;
+
+    private JLabel firstNameJL;
+    private JTextField firstNameTF;
+
+    private JLabel lastNameJL;
+    private JTextField lastNameTF;
+
+    private JLabel pNumberJL;
+    private JTextField pNumberTF;
+
+    private JButton createProfileButton;
+
+    public CreateProfilePanel() {
+        this.setLayout(new GridLayout(5, 2));
+        addComponents();
+
+    }
+
+    public void addComponents() {
+
+        selectCourseJL = new JLabel("Course:");
+        selectCourseTF = new JTextField();
+        add(selectCourseJL);
+        add(selectCourseTF);
+
+        firstNameJL = new JLabel("First Name:");
+        firstNameTF = new JTextField();
+        add(firstNameJL);
+        add(firstNameTF);
+
+        lastNameJL = new JLabel("Last Name:");
+        lastNameTF = new JTextField();
+        add(lastNameJL);
+        add(lastNameTF);
+
+        pNumberJL = new JLabel("P Number:");
+        pNumberTF = new JTextField();
+        add(pNumberJL);
+        add(pNumberTF);
+
+        createProfileButton = new JButton("Create Profile");
+        add(createProfileButton);
+
+
+
+    }
+
 }
--- src/GUIController.java
+++ src/GUIController.java
@@ -1,10 +1,48 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by Will on 11/04/2015.
  */
 public class GUIController {
 
+    private GUIModel model;
+    private GUIView view;
+
     public GUIController(GUIModel model, GUIView view) {
 
+        this.model = model;
+        this.view = view;
+
+        view.addLoadActionListener(new LoadActionListener());
+        view.addSaveActionListener(new SaveActionListener());
+        view.addExitActionListener(new ExitActionListener());
     }
+
+    class LoadActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class SaveActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class ExitActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+
 
 }
